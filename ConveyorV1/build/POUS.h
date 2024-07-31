3f2284d78741334f4a30244c5af8690ef2fc2cb5
TravisAnde <92126044+TravisAnde@users.noreply.github.com>
2024-07-31 11:09:58 +1000

    Add files via upload

---
+++ ConveyorV1/build/POUS.h
@@ -0,0 +1,135 @@
+#include "beremiz.h"
+#ifndef __POUS_H
+#define __POUS_H
+
+#include "accessor.h"
+#include "iec_std_lib.h"
+
+__DECLARE_ENUMERATED_TYPE(LOGLEVEL,
+  LOGLEVEL__CRITICAL,
+  LOGLEVEL__WARNING,
+  LOGLEVEL__INFO,
+  LOGLEVEL__DEBUG
+)
+// FUNCTION_BLOCK LOGGER
+// Data part
+typedef struct {
+  // FB Interface - IN, OUT, IN_OUT variables
+  __DECLARE_VAR(BOOL,EN)
+  __DECLARE_VAR(BOOL,ENO)
+  __DECLARE_VAR(BOOL,TRIG)
+  __DECLARE_VAR(STRING,MSG)
+  __DECLARE_VAR(LOGLEVEL,LEVEL)
+
+  // FB private variables - TEMP, private and located variables
+  __DECLARE_VAR(BOOL,TRIG0)
+
+} LOGGER;
+
+void LOGGER_init__(LOGGER *data__, BOOL retain);
+// Code part
+void LOGGER_body__(LOGGER *data__);
+// FUNCTION_BLOCK PYTHON_EVAL
+// Data part
+typedef struct {
+  // FB Interface - IN, OUT, IN_OUT variables
+  __DECLARE_VAR(BOOL,EN)
+  __DECLARE_VAR(BOOL,ENO)
+  __DECLARE_VAR(BOOL,TRIG)
+  __DECLARE_VAR(STRING,CODE)
+  __DECLARE_VAR(BOOL,ACK)
+  __DECLARE_VAR(STRING,RESULT)
+
+  // FB private variables - TEMP, private and located variables
+  __DECLARE_VAR(DWORD,STATE)
+  __DECLARE_VAR(STRING,BUFFER)
+  __DECLARE_VAR(STRING,PREBUFFER)
+  __DECLARE_VAR(BOOL,TRIGM1)
+  __DECLARE_VAR(BOOL,TRIGGED)
+
+} PYTHON_EVAL;
+
+void PYTHON_EVAL_init__(PYTHON_EVAL *data__, BOOL retain);
+// Code part
+void PYTHON_EVAL_body__(PYTHON_EVAL *data__);
+// FUNCTION_BLOCK PYTHON_POLL
+// Data part
+typedef struct {
+  // FB Interface - IN, OUT, IN_OUT variables
+  __DECLARE_VAR(BOOL,EN)
+  __DECLARE_VAR(BOOL,ENO)
+  __DECLARE_VAR(BOOL,TRIG)
+  __DECLARE_VAR(STRING,CODE)
+  __DECLARE_VAR(BOOL,ACK)
+  __DECLARE_VAR(STRING,RESULT)
+
+  // FB private variables - TEMP, private and located variables
+  __DECLARE_VAR(DWORD,STATE)
+  __DECLARE_VAR(STRING,BUFFER)
+  __DECLARE_VAR(STRING,PREBUFFER)
+  __DECLARE_VAR(BOOL,TRIGM1)
+  __DECLARE_VAR(BOOL,TRIGGED)
+
+} PYTHON_POLL;
+
+void PYTHON_POLL_init__(PYTHON_POLL *data__, BOOL retain);
+// Code part
+void PYTHON_POLL_body__(PYTHON_POLL *data__);
+// FUNCTION_BLOCK PYTHON_GEAR
+// Data part
+typedef struct {
+  // FB Interface - IN, OUT, IN_OUT variables
+  __DECLARE_VAR(BOOL,EN)
+  __DECLARE_VAR(BOOL,ENO)
+  __DECLARE_VAR(UINT,N)
+  __DECLARE_VAR(BOOL,TRIG)
+  __DECLARE_VAR(STRING,CODE)
+  __DECLARE_VAR(BOOL,ACK)
+  __DECLARE_VAR(STRING,RESULT)
+
+  // FB private variables - TEMP, private and located variables
+  PYTHON_EVAL PY_EVAL;
+  __DECLARE_VAR(UINT,COUNTER)
+  __DECLARE_VAR(UINT,ADD10_OUT)
+  __DECLARE_VAR(BOOL,EQ13_OUT)
+  __DECLARE_VAR(UINT,SEL15_OUT)
+  __DECLARE_VAR(BOOL,AND7_OUT)
+
+} PYTHON_GEAR;
+
+void PYTHON_GEAR_init__(PYTHON_GEAR *data__, BOOL retain);
+// Code part
+void PYTHON_GEAR_body__(PYTHON_GEAR *data__);
+// PROGRAM BLANK_PROJECT
+// Data part
+typedef struct {
+  // PROGRAM Interface - IN, OUT, IN_OUT variables
+
+  // PROGRAM private variables - TEMP, private and located variables
+  __DECLARE_VAR(BOOL,STARTBUTTON)
+  __DECLARE_VAR(BOOL,SAFEDOOR)
+  __DECLARE_VAR(BOOL,BOX_NOT_WAITING)
+  __DECLARE_VAR(BOOL,PICKER_HOLDING)
+  __DECLARE_VAR(BOOL,PICKER_ROTATING)
+  __DECLARE_VAR(BOOL,PICKER_MOVING_Z)
+  __DECLARE_VAR(BOOL,CONVEYOR_MOVE)
+  __DECLARE_VAR(BOOL,PICKER_MOVE_DOWN)
+  __DECLARE_VAR(BOOL,PICKER_GRAB)
+  __DECLARE_VAR(BOOL,PICKER_ROTATE_CCW)
+  __DECLARE_VAR(BOOL,PICKER_ROTATE_CW)
+  __DECLARE_VAR(BOOL,RUN)
+  F_TRIG F_TRIG1;
+  F_TRIG F_TRIG2;
+  R_TRIG R_TRIG1;
+  F_TRIG F_TRIG3;
+  F_TRIG F_TRIG4;
+  F_TRIG F_TRIG5;
+  F_TRIG F_TRIG6;
+  F_TRIG F_TRIG7;
+
+} BLANK_PROJECT;
+
+void BLANK_PROJECT_init__(BLANK_PROJECT *data__, BOOL retain);
+// Code part
+void BLANK_PROJECT_body__(BLANK_PROJECT *data__);
+#endif //__POUS_H
